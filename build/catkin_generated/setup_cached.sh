2a7d8641f4f8a1c4f1c2ffada39dcbc7efc972ab
caffedrine <bacchusstyle@yahoo.com>
2017-03-30 22:15:00 +0200

    Updated sources with better comments

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -10,6 +10,7 @@
 export CMAKE_PREFIX_PATH="/home/curiosul/ROS_Workspace/sandbox/roi/build/devel:/home/curiosul/opt/Qt/5.8/gcc_64"
 export CPATH="/home/curiosul/opt/Qt/5.8/gcc_64/include"
 export LD_LIBRARY_PATH="/home/curiosul/opt/Qt/5.8/gcc_64/lib"
-export PATH="/home/curiosul/opt/Qt/5.8/gcc_64/bin:/scripts:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games"
+export PATH="/home/curiosul/opt/Qt/5.8/gcc_64/bin:/usr/bin:/scripts:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games"
 export PKG_CONFIG_PATH="/home/curiosul/opt/Qt/5.8/gcc_64/lib/pkgconfig"
+export PWD="/home/curiosul/ROS_Workspace/sandbox/roi/build"
 export PYTHONPATH=""
--- src/roisub.cpp
+++ src/roisub.cpp
@@ -7,25 +7,37 @@
 
 #include <iostream>
 
-void image_callback(const roi::sendMsg &msg)
+void image_callback(const roi::sendMsg &recvMsg)
 {
     try
     {
-       //afisare frame_id
-       std::cout<< "[" << msg.image.header.frame_id << "]";
-       long t_now = ros::Time::now().toNSec();
-       long t_recv = msg.image.header.stamp.toNSec();
-       std::cout << "[" << (double)(t_now - t_recv)/1e6 << " ms] ";
-       std::cout <<"[" << msg.image.header.seq << "] " << std::endl;
+        //Display how much time was neccessary for this message to reach this node
+        long t_now = ros::Time::now().toNSec();
+        long t_recv = recvMsg.image.header.stamp.toNSec();
+        std::cout << "[" << (double)(t_now - t_recv)/1e6 << " ms] ";
 
-        cv_bridge::CvImagePtr cvImg = cv_bridge::toCvCopy(msg.image, "bgr8");
+        //Display transmission sequence data
+        std::cout <<"[SEQ: " << recvMsg.image.header.seq << "]";
+
+        //Display frame ID data
+        std::cout << "[F_ID: " << recvMsg.image.header.frame_id << "]";
+
+        //Display some IMU data
+        std::cout << "[IMU X:" << recvMsg.imu.imu.angular_velocity.x << " ";
+        std::cout << "Y: " << recvMsg.imu.imu.angular_velocity.y << " ";
+        std::cout << "Z: " << recvMsg.imu.imu.angular_velocity.z << "]";
+
+        //Display ROIs
+        std::cout << "[Rois_nr: " << recvMsg.rois.nr << "]" << std::endl;
+
+        cv_bridge::CvImagePtr cvImg = cv_bridge::toCvCopy(recvMsg.image, "bgr8");
 
         cv::imshow("view", cvImg->image);
         cv::waitKey(30);
     }
     catch (cv_bridge::Exception& e)
     {
-        std::cout <<"Error: [" << msg.image.header.seq << "]" << std::endl;
+        std::cout <<"Error: [" << recvMsg.image.header.seq << "]" << std::endl;
        //ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
     }
 }
